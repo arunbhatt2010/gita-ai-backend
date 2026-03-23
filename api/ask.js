23510820c6f0c261099d4bf06d7b40988e6a451c
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-03-23 14:10:20 +0530

    Finish

--- api/ask.js
+++ api/ask.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     }
 
     return res.status(200).json({
-      reply: data.choices[0].message.content
+    reply: JSON.stringify(data)
     });
 
   } catch (error) {
